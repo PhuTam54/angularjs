1b9a18bf50ff93581d742d67186041102dca6014
PhuTam54 <phutamytb@gmail.com>
2023-02-02 17:11:54 +0700

    slot 3

--- Ex1_ajs-form-validation/js/Ex1_ajs.js
+++ Ex1_ajs-form-validation/js/Ex1_ajs.js
@@ -1,7 +1,7 @@
-var validationApp = angular.module('validationApp',[]);
-validationApp.controller('mainController',function ($scope){
-    $scope.submitForm = function (isValid){
-        if(isValid){
+var validationApp = angular.module('validationApp', []);
+validationApp.controller('mainController', function ($scope) {
+    $scope.submitForm = function (isValid) {
+        if (isValid) {
             alert('form is valid');
         }
     };
